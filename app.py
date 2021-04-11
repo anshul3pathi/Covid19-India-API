8b55119013507ec9c014f5fc2a21ba943bdd3be1
Anshul Tripathi <anshul.3pathi@gmail.com>
2021-04-12 02:07:00 +0530

    selenium test-8

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def should_fetch():
 			except ValueError:
 				return True
 	except FileNotFoundError:
-		db.create_all()
+		with app.app_context():
+			db.create_all()
 		with open("should_fetch.txt", 'w') as file:
 			file.write(str(datetime.now()))
 		return True
