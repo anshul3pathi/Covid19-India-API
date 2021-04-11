3f4e27b493a5f2a0b1892ea0d95ebbfc76a3470f
Anshul Tripathi <anshul.3pathi@gmail.com>
2021-04-12 02:46:41 +0530

    we did it mr stark, selenium is working

--- test_app.py
+++ test_app.py
@@ -2,7 +2,9 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE_URL + "statecoviddata")
+DEPLOYED_BASE_URL = "https://covid19indiaorgapi.herokuapp.com/"
+
+response = requests.get(DEPLOYED_BASE_URL + "statecoviddata")
 array = response.json()
 print(array)
 print(len(array))
